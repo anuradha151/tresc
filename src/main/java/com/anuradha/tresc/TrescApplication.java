3ea58229f3a98620cb195c6fa02dce4cdfe1ac94
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2023-02-12 11:31:10 +0530

    Initialize project with web dependancy

---
+++ src/main/java/com/anuradha/tresc/TrescApplication.java
@@ -0,0 +1,13 @@
+package com.anuradha.tresc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrescApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrescApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anuradha/tresc/TrescApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuradha.tresc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrescApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
